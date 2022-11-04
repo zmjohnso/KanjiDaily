fdc212b8a5cb8b2f9bf77755ac1d4e336805f2a4
Zachary Johnson <zachary.johnson@atomicobject.com>
2022-11-04 13:26:49 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import random
+
+import requests
+import telebot
+from decouple import config
+
+API_KEY = config('API_KEY_TG')
+bot = telebot.TeleBot(API_KEY, parse_mode=None)
+
+kanjiapi_url = 'kanjiapi.dev'
+
+@bot.message_handler(commands=['kanji'])
+def kanji(message):
+    response = requests.get(f'https://{kanjiapi_url}/v1/kanji/joyo')
+    if response.status_code == 200:
+        length = len(response.json())
+        random_kanji = response.json()[random.randint(0, length - 1)]
+        meaning = requests.get(f'https://{kanjiapi_url}/v1/kanji/{random_kanji}')
+        if meaning.status_code == 200:
+            random_kanji_meaning = meaning.json()['meanings']
+            full_message = f'{random_kanji}: '
+            for val in random_kanji_meaning:
+                full_message += val + ', '
+            # remove trailing ', '
+            full_message = full_message[:-2]
+            bot.send_message(message.chat.id, full_message)
+
+bot.infinity_polling()
+
+
+# @bot.message_handler(commands=['meaning'])
+# def kanji(message):
+#     response = requests.get(f'https://{kanjiapi_url}/v1/kanji/joyo')
+#     if response.status_code == 200:
+#         length = len(response.json())
+#         random_kanji = response.json()[random.randint(0, length - 1)]
+#         bot.send_message(message.chat.id, random_kanji)
